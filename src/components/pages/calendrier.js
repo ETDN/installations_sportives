2dc01c7980e623defd12947bc79cbaf697556c8d
etdnp <etdn.prod@gmail.com>
2023-06-06 10:04:14 +0200

    popup working

--- src/components/pages/calendrier.js
+++ src/components/pages/calendrier.js
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "../../css/calendrier.css";
 
-function Calendrier() {
+function Calendrier({ onDateSelect }) {
   const [date, setDate] = useState(new Date());
+
+  const handleDateChange = (selectedDate) => {
+    setDate(selectedDate);
+    onDateSelect(selectedDate.toDateString()); // Formater la date en tant que chaîne de caractères
+  };
 
   return (
     <div className="react-calendar">
       <div className="calendar-container">
-        <Calendar onChange={setDate} value={date} />
+        <Calendar onChange={handleDateChange} value={date} />
       </div>
       <p className="text-center">
         <span className="bold">Selected Date:</span> {date.toDateString()}
